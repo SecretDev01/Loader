3c827fe927d6a2297e9f3ffbd4cb4c17e4ba28c3
SecretDev01 <ugirecode@gmail.com>
2026-07-10 05:03:53 +0800

    Add support for Murder Mystery game

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 18794863104 then
     loadstring(game:HttpGet("https://script.ugirecode.workers.dev/demonology"))()
 elseif game.PlaceId == 11379739543 then
     loadstring(game:HttpGet("https://script.ugirecode.workers.dev/time%20bomb%20duel"))()
+elseif game.PlaceId == 142823291 then
+    loadstring(game:HttpGet("https://script.ugirecode.workers.dev/murder%20mystery"))()
 else
     warn("Unsupported game: " .. game.PlaceId)
 end
