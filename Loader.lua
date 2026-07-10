f1152a03a0180e0c2cee2a1bc82389e62d1ef43d
SecretDev01 <ugirecode@gmail.com>
2026-07-10 11:42:17 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 11379739543 then
     loadstring(game:HttpGet("https://script.ugirecode.workers.dev/time%20bomb%20duel"))()
 elseif game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://script.ugirecode.workers.dev/murder%20mystery"))()
+elseif game.PlaceId == 93044798454681 or game.PlaceId == 125810438250765 then
+    loadstring(game:HttpGet("https://script.ugirecode.workers.dev/deadly%20delivery"))()
 else
     warn("Unsupported game: " .. game.PlaceId)
 end
